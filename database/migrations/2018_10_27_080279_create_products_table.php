b61c07f43cc18c7b812e2e3006fc4eb356130dbd
sjuliper7 <sjuliper7@gmail.com>
2018-12-12 16:56:00 +0700

    add changes

--- database/migrations/2018_10_27_080279_create_products_table.php
+++ database/migrations/2018_10_27_080279_create_products_table.php
@@ -10,7 +10,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->double('price');
             $table->integer('stock');
             $table->string('images');
