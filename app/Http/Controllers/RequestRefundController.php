f14c1e907e6e7f39e0c297771a5e6b9cb2c62d2c
Gideon Panjaitan <gideonpanjaitan4@gmail.com>
2018-12-17 20:53:51 +0700

    Redesign database for refund and Structure Design

---
+++ app/Http/Controllers/RequestRefundController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\RequestRefund;
+use Illuminate\Http\Request;
+use App\CategoryProduct;
+use App\Transaction;
+
+class RequestRefundController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $requestRefund = new RequestRefund();
+
+        return redirect('request-refund');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $categoryProducts = CategoryProduct::all();
+        $product = Product::find($id);
+        return view('refund.request-refund', compact('categoryProducts' ,'product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/RequestRefund.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RequestRefund extends Model
+{
+    protected $fillable = [
+        'id_user', 'id_product', 'id_detail_transaction', 'id_status_refund', 'alasan_pengembalian',
+        'keterangan', 'bukti_barang_image', 'jumlah',
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User','id_user');
+    }
+
+    public function product(){
+        return $this->belongsTo('App\Product','id_product');
+    }
+
+    public function statusRefund(){
+        return $this->belongsTo('App\StatusRefund', 'id_status_refund');
+    }
+
+    public function detailTransaction(){
+        return $this->belongsTo('App\DetailTransaction', 'id_detail_transaction');
+    }
+
+    public function konfirmasiRefund(){
+        return $this->hasOne('App\KonfirmasiRefund','id_request_refund');
+    }
+
+    public function refunds(){
+        return $this->hasOne('App\Refund','id_request_refund');
+    }
+}
