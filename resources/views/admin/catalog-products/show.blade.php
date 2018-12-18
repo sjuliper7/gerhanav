3cfc9362675c4be38f11893552fbc7330b540677
jhon <if415011@students.del.ac.id>
2018-12-18 11:35:07 +0700

    discount product using modal

--- resources/views/admin/catalog-products/show.blade.php
+++ resources/views/admin/catalog-products/show.blade.php
@@ -37,24 +37,65 @@
 
                     <hr>
 
-                    <a href="{{ url()->previous() }}" class="btn btn-primary">Kembali</a>
+                    <a href="{{ url()->previous() }}" class="btn btn-primary">Back</a>
                     {{--@can('Edit Post')--}}
-                    <a href="{{ route('catalog-products.edit', $product->id) }}" class="btn btn-info" role="button">Ubah</a>
+                    {{--<a href="{{ route('catalog-products.edit', $product->id) }}" class="btn btn-info" role="button">Ubah</a>--}}
 
-                    <form action="{{url('/add-catalog/'.$product->id)}}" method="POST" style="margin-top:-2.7em;margin-left: 12.5em">
+                    <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Add Discount</button>
+                    <form action="{{url('/add-catalog/'.$product->id)}}" method="POST" style="margin-top:-2.6em;margin-left: 14.5em">
                         {{csrf_field()}}
-                        <input value="Tambahkan ke Katalog" type="submit" onclick="save()" class="btn btn-info" role="button">
+                        <input value="Add to Catalog" type="submit" onclick="save()" class="btn btn-info" role="button">
                     </form>
 
 
+                    <form action="{{url('/products/discount/'.$product->id)}}" method="post" id="myForm" data-toggle="validator" role="form" enctype="multipart/form-data">
+                        <input type="hidden" name="_method" value="POST ">
+                        {{ csrf_field() }}
+                    <div class="modal fade" id="myModal" role="dialog">
+                        <div class="modal-dialog">
 
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Discount Product</h4>
+                                </div>
+                                <div class="modal-body">
+                                        <div class="form-row">
+                                            <div class="form-group col-md-1">
+                                                <p class="medium"></p>
+                                            </div>
+                                            <div class="form-group col-md-8">
+                                                <p class="medium" style="margin-top: 10px;">Discount product saat ini {{$product->discount}}%, ubah menjadi</p>
+                                                <div class="help-block with-errors"></div>
+                                            </div>
+                                            <div class="form-group col-md-2" style="margin-left: -40px;">
+                                                <input type="text" name="discount" value="{{$product->discount}}"  class="form-control border-input" data-error="Please enter name" required>
+                                                <div class="help-block with-errors"></div>
+                                            </div>
+                                            <div class="form-group col-md-1" style="margin-top: 8px;margin-left: -55px;">
+                                                <p class="medium">%</p>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="modal-footer" style="margin-top: 50px;">
+                                            <input type="submit" id="add" value="Ubah Discount" class="btn btn-info" style="margin-top: 10px;">
+                                        </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    </form>
 
                 </div>
-
 
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
 
     <script type="text/javascript">
         $(document).ready(function () {
