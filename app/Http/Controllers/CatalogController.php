8f262010043530200dd72a40d1828c44eecf016b
sjuliper7 <sjuliper7@gmail.com>
2018-12-23 22:01:07 +0700

    renew permission

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -15,6 +15,10 @@ class CatalogController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct() {
+        $this->middleware(['auth', 'isAdmin']);//isAdmin middleware lets only users with a //specific permission permission to access these resources
+    }
+
     public function index()
     {
         $categories = CategoryProduct::all();
