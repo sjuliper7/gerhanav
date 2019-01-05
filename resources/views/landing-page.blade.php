7460b1bd95b0ac26d5c4b7a7e05e77407f1d8770
sjuliper7 <sjuliper7@gmail.com>
2019-01-05 11:13:57 +0700

    fixing images for banner

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -417,14 +417,14 @@
 
 @section('content')
     <div class="banner">
-        <div class="banner_background" style="background-image:url('/public/template/images/banner_background.jpg')"></div>
+        <div class="banner_background" style="background-image :url('/img/banner.png');"></div>
         <div class="container fill_height">
             <div class="row fill_height">
                 {{--<div class="banner_product_image"><img src="images/banner_product.png" alt=""></div>--}}
                 <div class="col-lg-5 offset-lg-4 fill_height">
                     <div class="banner_content">
-                        <h2 class="banner_text"style="color: #8b0000">new era of </h2>
-                        <h4 class="banner_text" style="margin-bottom: 150px;color: #8b0000">Kerajinan Tangan Tanah Batak</h4>
+                        <h2 class="banner_text"style="color: white">new era of </h2>
+                        <h4 class="banner_text" style="margin-bottom: 150px;color: white">Kerajinan Tangan Tanah Batak</h4>
                         {{--<div class="banner_price"><span>$530</span>$460</div>--}}
                         {{--<div class="banner_product_name">Apple Iphone 6s</div>--}}
                         {{--<div class="button banner_button"><a href="#">Shop Now</a></div>--}}
